1ec09fcb4d65ad0d2748190b4e58b7e8c2f5c565
porco <charlierahal@gmail.com>
2020-12-28 21:37:36 +0000

    updated run_union_example and saves out (/w ll)

--- src/python/main.py
+++ src/python/main.py
@@ -2,19 +2,19 @@ import os
 import numpy as np
 import pandas as pd
 from load_data import load_union_data, save_union_data
-from analysis import run_ols_sm, get_mspace, make_robust
+from analysis import run_full_ols, get_mspace, make_robust
 
 
 def run_union_example(d_path):
     y, x, c = load_union_data(os.path.join(d_path, 'input'))
-    beta_full = run_ols_sm(y, pd.merge(x, c,
-                           how='left', left_index=True,
-                           right_index=True))
+    beta_full = run_full_ols(y, pd.merge(x, c,
+                             how='left', left_index=True,
+                             right_index=True))
     control_list = c.columns.to_list()
     model_space = get_mspace(control_list)
-    beta, p, aic, bic = make_robust(y, x, c, model_space,
-                                    'bic', len(model_space))
-    save_union_data(beta, p, aic, bic, d_path)
+    beta, p, aic, bic, ll = make_robust(y, x, c, model_space,
+                                        len(model_space))
+    save_union_data(beta, p, aic, bic, ll, d_path)
 
 
 def main():
