e136c3e34d2d25cc6f09e2da63bff36e54684dd9
crahal <charlierahal@gmail.com>
2022-10-12 22:03:45 +0100

    Some small increments and moving things around

--- src/python/test.py
+++ src/python/test.py
@@ -14,7 +14,7 @@ d_path = os.path.join('..', '..',)
 # y, c, x, control_list, model_space = prepare_union(os.path.join(d_path, 'data', 'input', 'nlsw88.dta'))
 # myrobust = OLSRobust(x, y)
 # beta, p, aic, bic = myrobust.fit(c=c, space=model_space, s=100, mode='simple')
-# save_myrobust(beta, p, aic, bic, os.path.join(d_path, 'intermediate'), 'union_example')
+# save_myrobust(beta, p, aic, bic, os.path.join(d_path, 'data', 'intermediate'), 'union_example')
 
 # load arrays to prevent the above having to be run multiple times
 # '@TODO write .utils func to load this in and then do selection and weighting
@@ -23,5 +23,5 @@ d_path = os.path.join('..', '..',)
 #y, c, x, control_list, model_space = prepare_asc(os.path.join(d_path, 'data', 'input', 'CleanData_LASpending.dta'))
 #myrobust_panel = OLSRobust(x, y)
 #beta, p, aic, bic = myrobust_panel.fit(c=c, space=model_space, s=10, mode='panel')
-#save_myrobust(beta, p, aic, bic, os.path.join(d_path, 'intermediate'), 'ASC_example')
+#save_myrobust(beta, p, aic, bic, os.path.join(d_path, 'data', 'intermediate'), 'ASC_example')
 # '@TODO write .utils func to load this in and then do selection and weighting
