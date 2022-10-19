6e1a03ba1469e42d38415713d7f3951b6cb73329
Daniel Valdenegro <dhvalden@gmail.com>
2022-10-19 18:17:16 +0100

    adding a draft of the plots we expect from the library

--- src/python/figures.py
+++ src/python/figures.py
@@ -1,3 +1,36 @@
+import pandas as pd
+import joypy
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+# Mock-ups
+
+data = pd.read_csv('./data/intermediate/union_example/betas.csv', header=None)
+
+
+data.median()
+
+data['mean'] = data.median(axis=1)
+
+data2 = data.sort_values(by=['mean'])
+
+data2.reset_index(drop=True, inplace=True)
+
+data2.drop(columns=['mean'], inplace=True)
+
+toplot = data2.sample(300).sort_index()
+
+joypy.joyplot(toplot.T,
+              overlap=1,
+              colormap=cm.OrRd_r,
+              linecolor='w',
+              linewidth=.5,
+              figsize=[8, 19],
+              ylabels=False,)
+plt.show()
+
 
 def main_figure(results):
     pass
+
